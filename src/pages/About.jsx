9651bbcd568f1598ed26cf61c85b76811bd4463e
githubdivyeshmpatil <githubdivyeshmpatil@gmail.com>
2025-07-02 12:18:03 +0530

    new updated

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -9,11 +9,20 @@ function About() {
 }, []);
   return (
    <>
-    <BackgroundSection
-        imageUrl={aboutbg}
-        className="h-[500px] p-10"
-      >
-      </BackgroundSection>
+    <div className="relative w-full">
+     {/* Background Image */}
+     <img
+       src={aboutbg}
+       alt="boosty bites"
+       className="w-full h-auto object-contain object-top z-0 relative"
+     />
+   
+     {/* Overlay (Positioned absolutely on top of image) */}
+     <div className="absolute  z-10"></div>
+   
+     {/* Content Section */}
+    
+   </div>
         <AboutCompany />
    </>
   )
